3112c96dc401dc0fddd662e67d40f63d556109bd
jetsom <mashaojie@pinefield-inc.com>
2022-07-14 14:01:13 +0800

    perf: 优化docker配置

--- docker.js
+++ docker.js
@@ -1,21 +1,25 @@
-// npm run build map-view
-// docker build  --platform=linux/x86_64 -f .cicd/Dockerfile -t 129.28.46.148:5000/prod-space-console-map-view:1.5.80 .
-// docker push 129.28.46.148:5000/prod-space-console-map-view:1.5.80
-
 const { execSync } = require('child_process')
 const colors = require('colors-console')
 const packageJsonConfig = require('./package.json')
 
 let status = true
 
-console.error(colors('blue',`----------生产环境：准备发布${packageJsonConfig.version}----------`))
+console.error(colors('blue',`----------测试环境：准备发布${packageJsonConfig.version}----------`))
 console.error(colors('blue',`----------开始编译----------`))
-runCmd("npm run build map-view")
-console.error(colors('blue',`----------开始打包镜像----------`))
-runCmd(`docker build  --platform=linux/x86_64 -f .cicd/Dockerfile -t 129.28.46.148:5000/prod-space-console-map-view:${packageJsonConfig.version} .`)
-console.error(colors('blue',`----------开始上传镜像----------`))
-runCmd(`docker push 129.28.46.148:5000/prod-space-console-map-view:${packageJsonConfig.version}`)
-console.error(colors('blue',`----------生产环境：发布完成【129.28.46.148:5000/prod-space-console-map-view:${packageJsonConfig.version}】----------`))
+runCmd("npm run buildtest")
+if(status){
+    console.error(colors('blue',`----------开始打包镜像----------`))
+    runCmd(`docker build  --platform=linux/x86_64 -f .cicd/Dockerfile -t 129.28.46.148:5000/nuclein-sampling-queue-phone:${packageJsonConfig.version} .`)
+} else {
+    console.error(colors('red',`----------项目编译失败，发布终止！----------`))
+}
+if(status){
+    console.error(colors('blue',`----------开始上传镜像----------`))
+    runCmd(`docker push 129.28.46.148:5000/nuclein-sampling-queue-phone:${packageJsonConfig.version}`)
+    console.error(colors('blue',`----------测试环境：发布完成【129.28.46.148:5000/nuclein-sampling-queue-phone:${packageJsonConfig.version}】----------`))
+} else {
+    console.error(colors('red',`----------项目打包镜像失败，发布终止！----------`))
+}
 
 function runCmd(cmd){
     execSync(cmd, { maxBuffer: 1024 * 1024 * 10 }, (error, stdout, stderr) => {
